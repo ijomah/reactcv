e28665d486ab77fc48aabfe699014a11f288387f
ijomah jacob <ijomahifinwa@gmail.com>
2023-02-08 09:41:56 -0800

    courses comp

--- src/Content/articlePart/carousals/courseCarousal.js
+++ src/Content/articlePart/carousals/courseCarousal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import  courseNGfunda from "../../../images/courseNGfunda.jpg";
 import  courseCss from "../../../images/courseCss.jpg";
 import  courseDSA from "../../../images/courseDSA.jpg";
@@ -11,82 +11,83 @@ import './courses.css';
 function Courses() {
     return (
         <div className="courses">
-            <div className="course">
-                <div className="course-head">
-                    <div className="course-title">from store</div>
-                </div>
-                <div className="course-body">
-                    <img src={courseNGfunda} alt="Completed Angular course badge"/>
-                    <p>Courses extract</p>
-                </div>
-                <div className="course-footer">
-                    <div>love icon <FontAwesomeIcon icon="fa-regular fa-heart" /></div>
-                </div>
-            </div>
 
             <div className="course">
                 <div className="course-head">
-                    <div className="course-title">from store</div>
-                </div>
-                <div className="course-body">
-                    <img src={courseCss} alt="" />
-                    <p>Courses extract</p>
-                </div>
-                <div className="course-footer">
-                    <div>love icon <FontAwesomeIcon icon="fa-regular fa-heart" /></div>
-                </div>
-            </div>
-
-            <div className="course">
-                <div className="course-head">
-                    <div className="course-title">from store</div>
+                    <div className="course-title">From PluralSight</div>
                 </div>
                 <div className="course-body">
                     <img src={courseDSA} alt="" />
-                    <p>Courses extract</p>
+                    {/* <p>Courses extract</p> */}
                 </div>
-                <div className="course-footer">
+                {/* <div className="course-footer">
                     <div>love icon <FontAwesomeIcon icon="fa-regular fa-heart" /></div>
-                </div>
+                </div> */}
             </div>
 
             <div className="course">
                 <div className="course-head">
-                    <div className="course-title">from store</div>
+                    <div className="course-title">From PluralSight  </div>
                 </div>
                 <div className="course-body">
-                    <img src={courseGit} alt=""></img>
-                    <p>Courses extract</p>
+                    <img src={courseNGfunda} alt="Completed Angular course badge"/>
+                    {/* <p>Courses extract</p> */}
                 </div>
-                <div className="course-footer">
+                {/* <div className="course-footer">
                     <div>love icon <FontAwesomeIcon icon="fa-regular fa-heart" /></div>
-                </div>
+                </div> */}
             </div>
 
             <div className="course">
                 <div className="course-head">
-                    <div className="course-title">from store</div>
+                    <div className="course-title">From PluralSight</div>
+                </div>
+                <div className="course-body">
+                    <img src={courseCss} alt="" />
+                    {/* <p>Courses extract</p> */}
+                </div>
+                {/* <div className="course-footer">
+                    <div>love icon <FontAwesomeIcon icon="fa-regular fa-heart" /></div>
+                </div> */}
+            </div>
+
+            <div className="course">
+                <div className="course-head">
+                    <div className="course-title">From PluralSight</div>
                 </div>
                 <div className="course-body">
                     <img src={courseFigma} alt=""></img>
-                    <p>Courses extract</p>
+                    {/* <p>Courses extract</p> */}
                 </div>
-                <div className="course-footer">
+                {/* <div className="course-footer">
                     <div>love icon <FontAwesomeIcon icon="fa-regular fa-heart" /></div>
-                </div>
+                </div> */}
             </div>
 
             <div className="course">
                 <div className="course-head">
-                    <div className="course-title">from store</div>
+                    <div className="course-title">From PluralSight</div>
+                </div>
+                <div className="course-body">
+                    <img src={courseGit} alt=""></img>
+                    {/* <p>Courses extract</p> */}
+                </div>
+                {/* <div className="course-footer">
+                    <div>love icon <FontAwesomeIcon icon="fa-regular fa-heart" /></div>
+                </div> */}
+            </div>
+
+            <div className="course">
+                <div className="course-head">
+                    <div className="course-title">From PluralSight</div>
                 </div>
                 <div className="course-body">
                     <img src={courseHTML} alt=""></img>
-                    <p>Courses extract</p>
+                    {/* <p>Courses extract</p> */}
                 </div>
-                <div className="course-footer">
+                {/* <div className="course-footer">
                     <div>love icon <FontAwesomeIcon icon="fa-regular fa-heart" /></div>
-                </div>
+                </div> */}
             </div>
 
         </div>
