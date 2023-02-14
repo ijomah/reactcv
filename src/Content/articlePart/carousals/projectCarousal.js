36fbc4b65d6ae1c37d5c09368edfd3ea740525e2
ijomah jacob <ijomahifinwa@gmail.com>
2023-02-14 12:16:14 -0800

    fixing images and github setup for projects

--- src/Content/articlePart/carousals/projectCarousal.js
+++ src/Content/articlePart/carousals/projectCarousal.js
@@ -1,20 +1,73 @@
 import React from "react";
+import './projects.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import weatherApp from '../../../images/weatherApp.jpg';
+import simpleWithAngular from '../../../images/simpleWithAngular.jpg';
+import twitterClone from '../../../images/twitterClone.jpg';
+import searchbarwithgoogle from '../../../images/searchbarwithgoogle.jpg';
 
 function ProjectStuff() {
     return (
-        <div>
-            <div className="proj">
+        <div className="projects">
+            <div className="proj1">
                 <div className="proj-head">
-                    <div className="proj-title">from store</div>
+                    <div className="proj-title"></div>
+                    <button ><Link to='https://cloudy-app-17494.web.app/'>Demo</Link></button>
+                    <button><Link to='https://github.com/ijomah/cloud_app'>Source Code</Link></button>
                 </div>
                 <div className="proj-body">
-                    <img src="" alt=""></img>
-                    <p>Prjects</p>
+                    <img src={weatherApp} loading="lazy" width="600" height="600" alt="Description of weather information by location"></img>
+                    {/* <p>Projects</p> */}
                 </div>
+                <div className="proj-footer">
+                <div> <FontAwesomeIcon icon="fa-regular fa-heart" /></div>
             </div>
-            <div className="proj-footer">
-                <div>love icon <FontAwesomeIcon icon="fa-regular fa-heart" /></div>
+            </div>
+            
+            <div className="proj2">
+                <div className="proj-head">
+                    <div className="proj-title"></div>
+                    <button ><Link to='https://ijomah.github.io/ijomahpaltiel/'>Demo</Link></button>
+                    <button><Link to='https://github.com/ijomah/ijomahpaltiel'>Source Code</Link></button>
+                </div>
+                <div className="proj-body">
+                    <img src={simpleWithAngular} loading="lazy" width="600" height="600" alt="Description of weather information by location"></img>
+                    {/* <p>Projects</p> */}
+                </div>
+                <div className="proj-footer">
+                <div> <FontAwesomeIcon icon="fa-regular fa-heart" /></div>
+            </div>
+            </div>
+
+            <div className="proj">
+                <div className="proj-head">
+                    <div className="proj-title"></div>
+                    <button ><Link to='https://cloudy-app-17494.web.app/'>Demo</Link></button>
+                    <button><Link to='https://github.com/ijomah/twitterLogin-clone'>Source Code</Link></button>
+                </div>
+                <div className="proj-body">
+                    <img src={twitterClone} loading="lazy" width="600" height="600" alt="Description of weather information by location"></img>
+                    {/* <p>Projects</p> */}
+                </div>
+                <div className="proj-footer">
+                <div> <FontAwesomeIcon icon="fa-regular fa-heart" /></div>
+            </div>
+            </div>
+
+            <div className="proj">
+                <div className="proj-head">
+                    <div className="proj-title"></div>
+                    <button ><Link to='https://cloudy-app-17494.web.app/'>Demo</Link></button>
+                    <button><Link to='https://github.com/ijomah/searchingbarwithgoogle'>Source Code</Link></button>
+                </div>
+                <div className="proj-body">
+                    <img src={searchbarwithgoogle} loading="lazy" width="600" height="600" alt="Description of weather information by location"></img>
+                    {/* <p>Projects</p> */}
+                </div>
+                <div className="proj-footer">
+                <div> <FontAwesomeIcon icon="fa-regular fa-heart" /></div>
+            </div>
             </div>
         </div>
     )
--- src/Content/articlePart/edu/educationCard.js
+++ src/Content/articlePart/edu/educationCard.js
@@ -6,7 +6,7 @@ import './education.css';
 const Educate = () => {
     return (
         <div>
-            <p><FontAwesomeIcon icon="fa-solid fa-user-graduate" className="graduate" size="2x" />Bachelors in Physics</p>
+            <p><FontAwesomeIcon icon="fa-solid fa-user-graduate" className="graduate" size="2x" />Bachelors in Physics (2<sup>nd</sup>  Class Upper Division)</p>
             <p><FontAwesomeIcon icon="fa-solid fa-school" className="school" size="2x" />Delta State University - Nigeria</p>
             <p><FontAwesomeIcon icon="fa-regular fa-calendar-days" className="edu-date" size="2x" />January 2006 to December 2009</p>
         </div>
